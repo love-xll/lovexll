af85757bc3ce07a04dcf2900d0b7147d457cdcc8
love-xll <130122665+love-xll@users.noreply.github.com>
2023-04-09 18:45:21 +0800

    Update talk.js

--- js/talk.js
+++ js/talk.js
@@ -1,7 +1,7 @@
 new TypeIt("#tqyxhj", {
         loop: true,
         cursorSpeed: 1000,
-        speed: 100
+        speed: 60
     })
     .type("哥哥 和 岚岚")
     .pause(2000)
@@ -16,5 +16,5 @@ new TypeIt('#talkToXHJ', {
     lifeLike: true,
     cursorSpeed: 1000,
     waitUntilVisible: true,
-    speed: 100
+    speed: 60
 }).go();
