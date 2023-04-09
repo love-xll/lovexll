19b985b4498837adff79f4449a73867aeb30080c
love-xll <zjn247191992@163.com>
2023-04-09 15:23:53 +0800

    Initial commit

---
+++ js/talk.js
@@ -0,0 +1,20 @@
+new TypeIt("#tqyxhj", {
+        loop: true,
+        cursorSpeed: 1000,
+        speed: 100
+    })
+    .type("哥哥 和 岚岚")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("一起走过的日子")
+    .pause(3000)
+    .go();
+
+new TypeIt('#talkToXHJ', {
+    lifeLike: true,
+    cursorSpeed: 1000,
+    waitUntilVisible: true,
+    speed: 100
+}).go();
